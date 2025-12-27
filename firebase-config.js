c9b84928f056fee832c758105d847b6a47892070
yuvrajlokhande19 <lokhandeyuvraj19@gmail.com>
2025-12-27 18:58:42 +0530

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4fps5XOnwVzCSiEuDR8MTIDBcj_o-m9w",
+    authDomain: "my-website-729ff.firebaseapp.com",
+    projectId: "my-website-729ff",
+    storageBucket: "my-website-729ff.firebasestorage.app",
+    messagingSenderId: "786071849682",
+    appId: "1:786071849682:web:78c3a990543a9af290fb0b",
+    measurementId: "G-18VM5SL3XC"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
